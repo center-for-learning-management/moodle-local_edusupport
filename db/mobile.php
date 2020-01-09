c16705ebe2a5f1e3529d07597ad5636db30dc9c0
Robert Schrenk <robert@schrenk.cc>
2020-01-09 08:12:30 +0100

    Tests for mobile capabilities.

---
+++ db/mobile.php
@@ -0,0 +1,49 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package    block_edusupport
+ * @copyright  2019 Digital Education Society (http://www.dibig.at)
+ * @author     Robert Schrenk
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+$addons = [
+    'block_edusupport' => [ // Plugin identifier
+        'handlers' => [ // Different places where the plugin will display content.
+            'issue_close' => [ // Handler unique name (alphanumeric).
+                'displaydata' => [
+                    'icon' => $CFG->wwwroot . '/mod/certificate/pix/icon.gif',
+                    'class' => 'block_edusupport',
+                    'title' => 'pluginname',
+                ],
+                'init' => 'edusupport_init',
+                'delegate' => 'CoreCourseModuleDelegate', // Delegate (where to display the link to the plugin)
+                'method' => 'issue_close', // Main function in \mod_certificate\output\mobile
+                'offlinefunctions' => [
+                    'mobile_course_view' => [],
+                    'mobile_issues_view' => [],
+                ], // Function that needs to be downloaded for offline.
+            ],
+        ],
+        'lang' => [ // Language strings that are used in all the handlers.
+            ['pluginname', 'block_edusupport'],
+            ['issue_close', 'block_edusupport'],
+        ],
+    ],
+];
