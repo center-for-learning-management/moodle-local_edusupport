30e132681e89a8d2eaaa829b76beb128031ef4de
Robert Schrenk <robert@schrenk.cc>
2021-08-30 16:27:44 +0200

    Increment version number

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version  = 2021080400;
+$plugin->version  = 2021083000;
 $plugin->requires = 2014051200;
 $plugin->component = 'local_edusupport';
 $plugin->release = '2.1';
